941cb7ad98c4877f13b59cf0b21845bb109ef58d
Hamdy Salah <hamdysalahmohamed.iti@gmail.com>
2024-12-12 00:08:26 +0200

    add home and profile

--- client/js/myorder.js
+++ client/js/myorder.js
@@ -1,39 +1,42 @@
-window.addEventListener("load",function(){
 
-    const userId = 1; //default value
-    const orderTableBody = document.querySelector('.table tbody');
+window.addEventListener("load", async function() {
+    let userId = localStorage.getItem("user_id"); 
+    if (!userId) {
+        console.error("User ID is missing");
+        document.getElementById('no-orders').classList.remove('d-none');  
+        return;
+    }
+    let orderList = await fetchUserOrders(userId);
+    populateOrder(orderList);
+});
 
-    async function populateOrders() {
-        try {
-          
-            const orders = await order.getUserOrdersHistory(userId);
+function populateOrder(orderList) {
+    let tableBody = document.querySelector('.tbody');
+    let noOrdersMessage = document.getElementById('no-orders');
+    
+    tableBody.innerHTML = "";  
 
-           
-            if (orders.length === 0) {
-                orderTableBody.innerHTML = ""
-                return;
-            }
-
-            orders.forEach((order, index) => {
-                order.products.forEach((product) => {
-                    const { name, price, quantity } = product;
-
-                   
-                    const row = `
-                        <tr>
-                            <td>${index + 1}</td>
-                            <td>${name}</td>
-                            <td>${price.toFixed(2)}</td>
-                            <td>${quantity}</td>
-                        </tr>`;
-                    orderTableBody.innerHTML += row;
-                });
-            });
-        } catch (error) {
-            console.error("Error fetching orders:", error);
-        }
+    if (orderList.length === 0) {
+        noOrdersMessage.classList.remove('d-none');  
+        return;
+    } else {
+        noOrdersMessage.classList.add('d-none');  
     }
 
-   
-    populateOrders();
-})//end of load
+    
+    orderList.forEach((order, index) => {
+        let formattedDate = new Date(order.created_at).toLocaleString();
+        let row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${order.first_name} ${order.second_name}</td>
+            <td>${order.email}</td>
+            <td>${order.address}, ${order.gov}</td>
+            <td>${order.phone_num1} / ${order.phone_num2}</td>
+            <td>${order.status === 1 ? 'Completed' : 'Pending'}</td>
+            <td>${formattedDate}</td>
+            <td>${order.message}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
