2228e4766a40f21aa21bcbf40df882c4a2119598
Sara Salem <sarasalem9898@outlook.com>
2024-12-24 19:08:01 +0200

    some buges fixed

--- client/js/general.js
+++ client/js/general.js
@@ -2,44 +2,42 @@
 import { cart } from '../../dashboard-assets/custome-js/Apis/cart.js';
 import { clientAuth } from '../../dashboard-assets/custome-js/Apis/Auth.js';
 
-export var genreal={
-    updateCartPill :async function () {
+export var genreal = {
+    updateCartPill: async function () {
 
-        let clientInfo=localStorage.getItem("clientSession")
-        if(clientInfo){
-
-            let usersession = JSON.parse(clientInfo);
-            let cartinfo=[];
-            let count=0;
-            let userId = null
-            
+        let clientInfo = localStorage.getItem("clientSession")
+        let usersession = JSON.parse(clientInfo);
+        let cartinfo = [];
+        let count = 0;
+        let userId = null
+        if (usersession) {
             userId = usersession.sessionData.id;
-           
-            if(userId){
 
-                cartinfo= await cart.getCartData(userId);
+        }
 
-            }else{
+        if (userId) {
 
-                cartinfo=JSON.parse(localStorage.getItem("user-cart"));
-                
-            }
-            count=0;
-            if(cartinfo){
-                cartinfo.products.forEach(function(p){
-                    count+=p['qty'];
-                })
-            }
-            var pill= document.getElementsByClassName("badge-pill")[0];
-            if(count>99){
-                pill.innerText='+99'    
-            }else{
+            cartinfo = await cart.getCartData(userId);
 
-                pill.innerText=count;
-            }
+        } else {
 
-        }//end of if
-        
+            cartinfo = JSON.parse(localStorage.getItem("user-cart"));
+
+        }
+        count = 0;
+        if (cartinfo) {
+            cartinfo.products.forEach(function (p) {
+                count += p['qty'];
+            })
+        }
+        var pill = document.getElementsByClassName("badge-pill")[0];
+        if (count > 99) {
+            pill.innerText = '+99'
+        } else {
+
+            pill.innerText = count;
+        }
+
 
     }//end of updateCartPill function.
 }
